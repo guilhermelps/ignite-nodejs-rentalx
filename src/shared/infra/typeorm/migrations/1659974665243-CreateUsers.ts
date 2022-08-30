17987ad5ed1ec0dc0f75c33b379bbc3dcfb35827
Guilherme Lopes <guilhermelopes17@hotmail.com>
2022-08-30 19:50:31 -0300

    criando a migration para a tabela de alugueis

--- src/shared/infra/typeorm/migrations/1659974665243-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1659974665243-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1659974665243 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
