2dcd5be4695b522e0480729b348958618a77061a
Guilherme Lopes <guilhermelopes17@hotmail.com>
2022-09-22 11:20:37 -0300

    refatorando autenticação de usuário

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "a8037c92c66374de169a9834428f38a5",
+  expires_in_token: "15m",
+  secret_refresh_token: "09b6377e997e0de3ce1233fffb7bdb9b",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
